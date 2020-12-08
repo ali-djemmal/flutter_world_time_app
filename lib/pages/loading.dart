567277bf3733fba98ed53f53576572a35e53f9c6
ali-djemmal <menaouerdj@gmail.com>
2020-12-08 14:17:45 +0100

    first commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_word_time/services/word_time.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time ="loading .....";
+
+
+  void setWorldTime() async{
+    WorldTime worldTime = WorldTime(location: 'Algiers',flag: 'algeria.png',url: 'Africa/Algiers');
+    await worldTime.getTime();
+    /*setState(() {
+      time= worldTime.time;
+    });*/
+   Navigator.pushReplacementNamed(context, '/home',arguments: {
+     'location': worldTime.location,
+     'time':worldTime.time,
+     'flag':worldTime.flag,
+     'isDayTime': worldTime.isDayTime
+   });
+
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Padding(
+          padding: const EdgeInsets.fromLTRB(0.0,150.0,0.0,0.0),
+          child: Column(
+          children: <Widget>[
+            SpinKitRotatingCircle(
+              color: Colors.black87,
+              size: 50.0,
+            ),
+            SizedBox(height: 20.0),
+            Text('Loading ...',
+            style: TextStyle(fontSize: 50.0),)
+          ],
+
+      ),
+        ))
+    );
+  }
+}
---
+++ lib/services/word_time.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location;
+  String time;
+  String flag;
+  String url;// api for  end point
+  bool isDayTime=null;
+
+  WorldTime({this.location,this.flag,this.url});
+
+  Future<void> getTime() async{
+    try{
+      Response response= await  get('http://worldtimeapi.org/api/timezone/$url');
+      Map data= jsonDecode(response.body);
+
+      // get proprties from data
+
+      String datatime = data['datetime'];
+      String offset =data['utc_offset'].substring(1,3);
+      DateTime now = DateTime.parse(datatime);
+      now= now.add(Duration(hours:int.parse(offset)));
+
+      isDayTime= now.hour>6 && now.hour<20 ? true : false;
+
+     // this.time= now.toString();
+      time = DateFormat.jm().format(now).toString();
+
+    } catch(e){
+      print('error : $e');
+      time='time error';
+
+    }
+    // make the request
+
+  }
+
+}
